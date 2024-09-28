e850b29b35ccd775a48074d5a611653dc138cab9
fri17da <syafridaadeauliahakim@gmail.com>
2024-09-28 17:52:12 +0700

    jobsheet5

---
+++ PemilihanOperator23.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PemilihanOperator23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+, -, *, /,): ");
+        operator = sc.next().charAt(0);
+
+        if (operator == "+") {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            if (angka2 != 0) {
+                hasil = angka1 / angka2;
+            } else {
+                System.out.println("Error: Pembagi tidak boleh nol.");
+                return;
+            }
+        } else {
+            System.out.println("Error: Operator yang dimasukkan tidak valid.");
+        }
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+    }
+}
