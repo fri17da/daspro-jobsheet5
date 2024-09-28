27c8f75f39b4b29a2e60aa8a15f69b792d2d1125
fri17da <syafridaadeauliahakim@gmail.com>
2024-09-28 16:37:51 +0700

    jobsheet5

--- PemilihanBilangan23.java
+++ PemilihanBilangan23.java
@@ -8,15 +8,8 @@ public class PemilihanBilangan23 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + " termasuk bilangan ganjil" );
-        }
-          
+        System.out.println("Angka " + (angka % 2 == 0 ? "Termasuk Bilangan Genap" : " Termasuk Bilangan Ganjil"));
+
       }
 }
     
